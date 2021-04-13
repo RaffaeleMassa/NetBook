60f6589b16c8d7301d10f9e712ebe109798ee123
RaffaeleMassa <76770497+RaffaeleMassa@users.noreply.github.com>
2021-04-13 17:50:37 +0200

    Create bookslist.js

---
+++ src/components/bookslist.js
@@ -0,0 +1,40 @@
+import React from "react"
+import {Link} from "react-router-dom"
+
+
+
+const Book = ({book}) =>{
+     
+    return(
+        <>
+              <li>
+                <div className="Tit">
+                 <img 
+                
+                 alt={`${book.volumeInfo.title} book`} 
+                 src={`http://books.google.com/books/content?id=${book.id}&printsec=frontcover&img=1&zoom=1&source=gbs_api`} />
+                <div>
+                 <h3 className="Tit">{book.volumeInfo.title}</h3>
+                 <p className="Tit">{book.volumeInfo.publishedDate}</p>
+                 <Link className="Tit" to={`/book/${book.id}`}>Show details</Link>
+               </div>
+               </div>
+               <hr />
+
+               
+              </li>
+            
+         </>
+            )
+    }
+
+    const BookList = ({books}) =>{
+        return(
+            <ul>
+                {books.items.map((book,index)=>{
+                    return <Book book ={book} key={index} />
+                })}
+            </ul>
+        )
+    }
+export default BookList;
